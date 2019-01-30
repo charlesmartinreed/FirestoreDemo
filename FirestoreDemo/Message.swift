6e8ab73da30f443292d4d1f62a8afaaa7b833af2
charlesmartinreed <charlesmartinreed@icloud.com>
2019-01-29 23:27:43 -0600

    Create Message data model. Create serialization protocol and extension on Message.

---
+++ FirestoreDemo/Message.swift
@@ -0,0 +1,38 @@
+//
+//  Message.swift
+//  FirestoreDemo
+//
+//  Created by Charles Martin Reed on 1/29/19.
+//  Copyright © 2019 Charles Martin Reed. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+protocol DocumentSerializable {
+    init?(dictionary: [String: Any])
+}
+
+struct Message {
+    var name: String
+    var content: String
+    var timeStamp: Date
+    
+    var dictionary: [String: Any] {
+        //computed property so we can call it later, to give Firestore what it needs - JSON
+        return [
+            "name": name,
+            "content": content,
+            "timeStamp": timeStamp
+        ]
+    }
+}
+
+extension Message : DocumentSerializable {
+    init?(dictionary: [String : Any]) {
+        guard let name = dictionary["name"] as? String, let content = dictionary["content"] as? String, let timeStamp = dictionary["timeStamp"] as? Date else { fatalError("Could not serialize document!") }
+        
+         self.init(name: name, content: content, timeStamp: timeStamp)
+    }
+}
+
--- FirestoreDemo/TableViewController.swift
+++ FirestoreDemo/TableViewController.swift
@@ -13,11 +13,26 @@ class TableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
     
     //MARK:- IBActions
     @IBAction func composeMessage(_ sender: UIBarButtonItem) {
+        let composeAlert = UIAlertController(title: "Compose New Message", message: "Enter your name and message", preferredStyle: .alert)
+        composeAlert.addTextField { (textField) in
+            textField.placeholder = "Your name"
+        }
+        composeAlert.addTextField { (textField) in
+            textField.placeholder = "Your message"
+        }
+        
+        composeAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        composeAlert.addAction(UIAlertAction(title: "Send", style: .default, handler: { (action) in
+            //here's where the Firebase magic happens
+        }))
+        
+        present(composeAlert, animated: true, completion: nil)
+        
     }
     
 
